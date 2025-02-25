ce2b96043bb9975f7040df5bce0d12f5d66cf12b
ireinahedad <ireina.hedad@polymtl.ca>
2025-02-25 13:15:18 -0500

    tp4 fini

--- TP4/src/scripts/legend.js
+++ TP4/src/scripts/legend.js
@@ -1,3 +1,4 @@
+import { color } from 'd3'
 import d3Legend from 'd3-svg-legend'
 
 /**
@@ -7,23 +8,40 @@ import d3Legend from 'd3-svg-legend'
  * @param {*} g The d3 Selection of the graph's g SVG element
  * @param {number} width The width of the graph, used to place the legend
  */
-export function drawLegend (colorScale, g, width) {
-  // TODO : Draw the legend using d3Legend
-  // For help, see : https://d3-legend.susielu.com/
+export function drawLegend(colorScale, g, width) {
   const legendGroup = g.append('g')
     .attr('class', 'legend')
-    .attr('transform', `translate(${width + 20}, -40)`) // Position the legend
+    .attr('transform', `translate(${width + 20}, -40)`);
 
-  console.log(colorScale.domain())
-  console.log(colorScale.range())
-  const legend = d3Legend.legendColor()
-    .scale(colorScale)
-    .shapePadding(2)
-    .shapeRadius(10)
-    .shape('circle')
-    .orient('vertical')
-    .labelAlign('start')
-    .title('Legend')
 
-  legendGroup.call(legend)
+
+    legendGroup.append("text")
+    .attr("class", "legendTitle")
+    .attr("x", 10)  // Position the title to the left
+    .attr("y", -20)  // Position above the symbols
+    .text("Legend")  // Replace with your desired title text
+    .style("font-size", "18px")
+
+
+    const symbol = d3.symbol().type(d3.symbolCircle).size(400);  
+
+    legendGroup.selectAll(".legendSymbols")
+      .data(colorScale.domain())
+      .enter()
+      .append("path")
+        .attr("d", symbol)  
+        .attr("transform", (d, i) => `translate(20, ${i * 25})`) 
+        .style("fill", d => colorScale(d));
+
+    // Add labels next to each symbol
+    legendGroup.selectAll(".legendLabels")
+      .data(colorScale.domain())
+      .enter()
+      .append("text")
+        .attr("x", 40)  
+        .attr("y", (d, i) => i * 25) 
+        .text(d => d)  
+        .style("font-size", "12px")
+        .style("fill", "#000");  
 }
+
