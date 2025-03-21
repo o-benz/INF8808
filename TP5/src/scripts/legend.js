3d611ffd8c730cf137a655c0d3dfa03110b9282c
FelixLamarche <felixlamarche28@gmail.com>
2025-03-21 13:17:11 -0400

    TP5 done

--- TP5/src/scripts/legend.js
+++ TP5/src/scripts/legend.js
@@ -9,4 +9,17 @@ import d3Legend from 'd3-svg-legend'
 export function drawLegend (colorScale, g) {
   // TODO : Generate the legend
   // For help, see : https://d3-legend.susielu.com/
+  const legendGroup = g.append('g')
+    .attr('class', 'legend')
+    .attr('transform', 'translate(50, 125)')
+    .style('font-family', 'Open Sans Condensed')
+    .style('font-size', '16px')
+
+  const legend = d3Legend.legendColor()
+    .title('Légende')
+    .shape('path', d3.symbol().type(d3.symbolCircle).size(400)())
+    .shapePadding(4)
+    .scale(colorScale)
+
+  legendGroup.call(legend)
 }
--- TP5/src/scripts/viz.js
+++ TP5/src/scripts/viz.js
@@ -6,7 +6,9 @@
  */
 export function colorDomain (color, data) {
   // Set the color domain
-  color.domain(data.features.map(d => d.properties.TYPE_SITE_INTERVENTION))
+  const types = data.features.map(d => d.properties.TYPE_SITE_INTERVENTION)
+  types.sort()
+  color.domain(types)
 }
 
 /**
@@ -18,9 +20,8 @@ export function colorDomain (color, data) {
  */
 export function mapBackground (data, path, showMapLabel) {
   // TODO : Generate the map background and set the hover handlers
-  d3.selectAll('.main-svg')
-    .selectAll('g')
-    .append('g')
+  d3.select('#map-g')
+    .selectAll('path')
     .data(data.features)
     .enter()
     .append('path')
@@ -71,7 +72,7 @@ export function mapMarkers (data, color, panel) {
   // Their color corresponds to the type of site and their outline is white.
   // Their radius is 5 and goes up to 6 while hovered by the cursor.
   // When clicked, the panel is displayed.
-  d3.selectAll('.main-svg')
+  d3.select('#marker-g')
     .selectAll('circle')
     .data(data.features)
     .enter()
@@ -80,15 +81,18 @@ export function mapMarkers (data, color, panel) {
     .attr('cx', d => d.x)
     .attr('cy', d => d.y)
     .attr('r', 5)
-    .attr('fill', d => color(d.properties.TYPE_SITE_INTERVENTION))
-    .attr('stroke', 'white')
-    .attr('stroke-width', 1)
+    .style('fill', d => color(d.properties.TYPE_SITE_INTERVENTION))
+    .style('stroke', 'white')
+    .style('stroke-width', 1)
+    .style('cursor', 'pointer')
     .on('mouseover', function () {
       d3.select(this).attr('r', 6)
     })
     .on('mouseout', function () {
       d3.select(this).attr('r', 5)
     })
-    .on('click', (e, d) => panel.display(d, color))
+    .on('click', (e, d) => {
+      panel.display(d, color)
+    })
     .raise()
 }
